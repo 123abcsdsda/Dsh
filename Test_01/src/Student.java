d09e5537ff450c7f20ffea8e9870dfc50283b71b
dongshuhao <3078720977@qq.com>
2024-01-23 15:36:26 +0800

    main第二次

---
+++ Test_01/src/Student.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Student {
+    public static void main(String[] args) {
+        String s = "asd";
+        List<String> list = new ArrayList<>();
+    }
+}
