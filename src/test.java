505fadd1bc2ecf0c4051ac4ed1cb29c9f9c1932c
dongshuhao <dongshuhao@autohome.com.cn>
2024-09-03 13:49:24 +0800

    dsa

---
+++ src/test.java
@@ -0,0 +1,12 @@
+/**
+ * @program: Program_01
+ * @description:
+ * @author: dongshuhao
+ * @create: 2024-09-03 13:48
+ **/
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
